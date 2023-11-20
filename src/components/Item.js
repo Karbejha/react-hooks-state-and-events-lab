602618876cd633831d8d104e59ffcb2402898b70
Mohamad Karbejha <78583194+Karbejha@users.noreply.github.com>
2023-11-21 00:05:55 +0300

    Solve the assignment , test it , working 100%

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // Use state to track whether the item is in the cart or not
+  const [inCart, setInCart] = useState(false);
+
+  // Function to handle adding or removing item from the cart
+  const handleToggleCart = () => {
+    setInCart(!inCart);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/* Change button text based on whether the item is in the cart or not */}
+      <button className="add" onClick={handleToggleCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
